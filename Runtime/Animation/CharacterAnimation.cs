2fabf43d9d8e9b75588fa179aa5a272e1a5cb444
Ryan Miller <me@ryanmiller.ca>
2025-11-27 10:36:22 -0500

    Adjust CharacterAnimation to support 2D sprites better with facing/direction params

--- Runtime/Animation/CharacterAnimation.cs
+++ Runtime/Animation/CharacterAnimation.cs
@@ -2,19 +2,56 @@ namespace RyanMillerGameCore.Animation
 {
     using UnityEngine;
     using System.Collections;
+    using Character;
 
     public class CharacterAnimation : MonoBehaviour
     {
+        [Header("References")]
         [SerializeField] private Animator animator;
+        
+        [Header("Params")]
         [SerializeField] private int cosmeticAnimLayer = 1;
         [SerializeField] private float cosmeticAnimDuration = 5;
         [SerializeField] private float blendTime = 0.25f;
-        private Coroutine _weightChanges;
         
-        public void PlayAnimation(string clipName, float customDuration = -1)
+        [Header("Direction/Facing")]
+        [Tooltip("Sends rotation/direction to animator as 0-3 value (0=N, 1=E, 2=S, 3=W")]
+        [SerializeField] private bool sendDirectionToAnimator = true;
+        [SerializeField] private string directionParameterName = "Direction";
+        [SerializeField] private int directionIndexOffset = 0;
+        [SerializeField] private float paramMultiplier = 0.1f;
+        [SerializeField] private float minThreshold = 0.001f;
+        private Coroutine _weightChanges;
+        private CharacterMovement _characterMovement;
+        
+        private void OnEnable()
         {
-            if (string.IsNullOrEmpty(clipName))
+            if (sendDirectionToAnimator) {
+                _characterMovement = GetComponent<CharacterMovement>();
+                _characterMovement.OnMoveInDirection += OnMoveInDirection;
+            }
+        }
+
+        private void OnDisable() {
+            if (sendDirectionToAnimator) {
+                _characterMovement.OnMoveInDirection -= OnMoveInDirection;
+            }
+        }
+
+        private void OnMoveInDirection(Vector3 inputDirection)
+        {
+            if (inputDirection.sqrMagnitude > minThreshold)
             {
+                float angle = Mathf.Atan2(inputDirection.z, inputDirection.x);
+                float angleDegrees = -angle * Mathf.Rad2Deg;
+                float normalizedAngle = Mathf.Repeat(angleDegrees, 360f);
+                float rotationIndex = (Mathf.FloorToInt(normalizedAngle / 90f) + 1) % 4;
+                animator.SetFloat(directionParameterName, rotationIndex * paramMultiplier);
+            }
+        }
+
+        public void PlayAnimation(string clipName, float customDuration = -1) {
+            if (string.IsNullOrEmpty(clipName)) {
                 return;
             }
             animator.Play(clipName, cosmeticAnimLayer, 0);
