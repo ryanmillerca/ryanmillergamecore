198eef7e2088de9749bc5cbbde0cc41079d3635e
Ryan Miller <me@ryanmiller.ca>
2025-12-04 09:47:56 -0500

    add mixmax range inspector/attribute

---
+++ Runtime/Utilities/MinMaxRangeAttribute.cs
@@ -0,0 +1,51 @@
+namespace RyanMillerGameCore.Utilities {
+	using System;
+	using UnityEngine;
+
+	/// <summary>
+	/// An attribute that simplifies defining bounded ranges (ranges with minimum and maximum limits) on the inspector.
+	/// From: https://github.com/lazysquirrellabs/min_max_range_attribute/blob/main/Assets/Lazy%20Squirrel%20Labs/MinMaxRangeAttribute/Scripts/MinMaxRangeAttribute.cs
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field)]
+	public class MinMaxRangeAttribute : PropertyAttribute {
+
+
+		#region Fields
+
+		public readonly float MinLimit;
+		public readonly float MaxLimit;
+		public readonly uint Decimals;
+
+		#endregion
+
+
+		#region Setup
+
+		/// <summary>
+		/// A bounded range for integers.
+		/// </summary>
+		/// <param name="minLimit">The minimum acceptable value.</param>
+		/// <param name="maxLimit">The maximum acceptable value.</param>
+		public MinMaxRangeAttribute(int minLimit, int maxLimit) {
+			MinLimit = minLimit;
+			MaxLimit = maxLimit;
+		}
+
+		/// <summary>
+		/// A bounded range for floats.
+		/// </summary>
+		/// <param name="minLimit">The minimum acceptable value.</param>
+		/// <param name="maxLimit">The maximum acceptable value.</param>
+		/// <param name="decimals">How many decimals the inspector labels should display. Values must be in the [0,3]
+		/// range. Default is 1.</param>
+		public MinMaxRangeAttribute(float minLimit, float maxLimit, uint decimals = 1) {
+			MinLimit = minLimit;
+			MaxLimit = maxLimit;
+			Decimals = decimals;
+		}
+
+		#endregion
+
+
+	}
+}
