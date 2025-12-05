66ae93bf8a96b1a337ef5d1a09aab3d007ad4be0
Ryan Miller <me@ryanmiller.ca>
2025-12-04 19:44:56 -0500

    Add InputActionReference option to be safe

--- Runtime/Input/TriggerFromAnyKey.cs
+++ Runtime/Input/TriggerFromAnyKey.cs
@@ -7,6 +7,7 @@ namespace RyanMillerGameCore.Input {
 
 	public class TriggerFromAnyKey : MonoBehaviour {
 
+		[SerializeField] private InputActionReference[] m_InputActionReferences;
 		[SerializeField] private UnityEvent AnyKeyPressed;
 		[SerializeField] private bool m_TriggerOnlyOnce = false;
 		private IDisposable m_EventListener;
@@ -15,10 +16,30 @@ namespace RyanMillerGameCore.Input {
 
 		private void OnEnable() {
 			m_EventListener = InputSystem.onAnyButtonPress.Call(OnButtonPressed);
+			if (m_InputActionReferences.Length > 0) {
+				foreach (var inputActionReference in m_InputActionReferences) {
+					inputActionReference.action.Enable();
+					inputActionReference.action.performed += OnButtonPressed;
+				}
+			}
 		}
 
 		private void OnDisable() {
 			m_EventListener.Dispose();
+			foreach (var inputActionReference in m_InputActionReferences) {
+				inputActionReference.action.Disable();
+				inputActionReference.action.performed -= OnButtonPressed;
+			}
+		}
+
+		public void OnButtonPressed(InputAction.CallbackContext context) {
+			if (m_TriggerOnlyOnce) {
+				if (m_DidTrigger) {
+					return;
+				}
+			}
+			AnyKeyPressed?.Invoke();
+			m_DidTrigger = true;
 		}
 
 		private void OnButtonPressed(InputControl button) {
